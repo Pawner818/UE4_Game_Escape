9a1ae7b6b456aef94de2e278d3d4307efd882515
Zakhar Hurov <gurov.zahar@gmail.com>
2020-02-25 14:28:22 +0100

    LineTraceSingleByObjectType()

--- Source/UE4_Escape_Game/Grabber.cpp
+++ Source/UE4_Escape_Game/Grabber.cpp
@@ -45,11 +45,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		OUT PlayerViewPointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("Location:%s, Rotation:%s"),
-		*PlayerViewPointLocation.ToString(),
-		*PlayerViewPointRotation.ToString()
-	);
-
 	// Draw a line from player showing the reach.
 
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
@@ -67,8 +62,29 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	// Logging out to test
 
+	FHitResult Hit;
+
 	// Ray-cast out to a certain distance (Reach)
 
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
 	// See what it hits
+
+	AActor* ActorHit = Hit.GetActor();
+
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Line trace has hit: %s"), *(ActorHit->GetName()))
+	}
+
+	// Logging out to test
 }
 
